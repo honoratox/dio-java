2750ff0fe3c54b365bc8cc4bd74ea7956e23fc63
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2024-07-02 19:00:21 -0300

    Add files via upload

---
+++ java-anatomia-classes-2/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse{
+    public static void main (String [] args) {
+
+        int ano = 2021;
+        ano = 2022;
+    }
+}
+// variáveis normalmente são minúsculas (ex: int ano), essas podem sofrer mutações
+// variáveis maiúsculas são imutáveis (ex: int ANO_2000 = 2000 || final String BR = "Brasil")
