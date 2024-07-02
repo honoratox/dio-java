9e827ef1323be1af345a9dffc181730e7377dd12
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2024-07-02 18:36:12 -0300

    Add files via upload

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse{
+    public static void main (String [] args) {
+        System.out.print ("Esse foi o teste de classe");
+    } //corpo somente do método main
+}
+
+//toda classe deve estar contida no diretório 'src'
+//toda classe tem que ter nome maiusculo (ex: Casa.java)
+//se for palavra composta, todo início deve ser maiúsculo (ex: MinhaCasa.java)
